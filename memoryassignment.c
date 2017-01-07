44da889b4dc6ca31f4a5e8bfa7fbed0d6b92f6b4
MohankumarR <mohankumarr023@gmail.com>
2017-01-07 14:30:59 +0530

    Create memoryassignment.c

---
+++ memoryassignment.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main(void) {
+	int a[]={0x31324142,0x41423132,0x64};
+	printf("%s",a);
+	return 0;
+}
+
+#include <stdio.h>
+
+int main(void) {
+    int y=0x3132,a=0x31324142,b=0x41423132,d=111222;
+     printf("%d\n",b);
+     printf("%d\n",a);
+      printf("%c\n",a);
+       printf("%c\n",b);
+        printf("%x\n",d);
+	return 0;
+}
