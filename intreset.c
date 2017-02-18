f29e0485710102a7e86fbebad6c7ca809f7bdce3
MohankumarR <mohankumarr023@gmail.com>
2017-02-18 14:41:47 +0530

    Update intreset.c

--- intreset.c
+++ intreset.c
@@ -29,3 +29,37 @@ int main()
 	rel=si(p,n,r);
 	printf("%f",rel);
 }/simple intrest
+#include<stdio.h>
+float si(int a,int b,float c)
+{
+	return((a*b*c)/(1200));
+}
+float add(float b[10],int l)
+{
+	float r=0;
+	for(int i=0;i<l;i++)
+	{
+		r=r+b[i];
+	}
+	return(r);
+}
+int main()
+{
+	int n,f,i,j,k;
+	float p,r,a[10],s=0,sum;
+	scanf("%f%f",&p,&r);
+	scanf("%d%d",&n,&f);
+	for(i=0;i<n/f;i++)
+	{
+		k=0;
+		for(j=0;j<f;j++)
+		{
+			s=s+p;
+			a[k]=si(s,1,r);
+			k++;
+		}
+		sum=add(a,k);
+		s=s+sum;
+	}
+	printf("%f",s);
+}/recuringdeposit
