a2ab9f5d3acec625608ad8fe71315845a3c2b8c3
MohankumarR <mohankumarr023@gmail.com>
2017-03-15 22:34:02 +0530

    Create linkedlist.c

---
+++ linkedlist.c
@@ -0,0 +1,78 @@
+#include <stdio.h>
+#include<stdlib.h>
+#include<malloc.h>
+struct node;
+struct node
+{
+    int data;
+    struct node *next;
+}*head;
+void create(int);
+void search(int);
+void display();
+int main(void) {
+	int n,key;
+	scanf("%d",&n);
+	create(n);
+	//display();//
+	scanf("%d",&key);
+	search(key);
+	return 0;
+}
+void create(int n)
+{
+    int i,d;
+    struct node *temp,*t1;
+    for(i=0;i<n;i++)
+    {
+        temp=(struct node *)malloc(sizeof(struct node));
+        scanf("%d",&d);
+        if(head==NULL)
+        {
+            temp ->data=d;
+            temp ->next=NULL;
+            head=temp;
+            t1=temp;
+        }
+        else
+        {
+            temp ->data=d;
+            temp ->next=NULL;
+            t1 ->next=temp;
+            t1=t1 ->next;
+        }
+    }
+}
+void search(int key)
+{
+    int count=0;
+    struct node *ptr;
+    ptr=head;
+    while(ptr!=NULL)
+    {
+        if(ptr ->data==key)
+        {
+            printf("%d\n",count);
+            printf("%u",ptr ->next);
+        }
+        ptr=ptr ->next;
+        count++;
+    }
+}
+void display()
+{
+    struct node *t;
+    t=head;
+    if(head==NULL)
+    {
+        printf("list is empty");
+    }
+    else
+    {
+        while(t!=NULL)
+        {
+            printf("%d",t ->data);
+            t=t->next;
+        }
+    }
+}
